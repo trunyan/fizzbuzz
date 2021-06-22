990e0332d2a94e48985eb48a3519275dc2122a6e
trunyan <trunyan@users.noreply.github.com>
2021-06-22 09:45:48 -0600

    Added NULL test and one more transformation 3 test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -26,11 +26,17 @@ class TestFizzBuzz(unittest.TestCase):
 
         self.assertEqual(output, expected_output)
 
+        expected_output = 'fizz: 1, buzz: 1, fizzbuzz: 1, lucky: 1, integer: 1'
+        output = fizzbuzz.fizz_buzz_report(['3', '6', '5', '1','15'])
+
+        self.assertEqual(output, expected_output)
+
+
     def test_row_validator(self):
         negative_input = ['-1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
         decimal_input = ['1.5', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
         spaces_input = ['1', '   2', '3    ', ' 4   ', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
-        oct_input = ['1.5', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+        null_input = ['None', 'null', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 
         #valid row tests
         self.assertEqual(fizzbuzz.validate_row(self.fizzbuzz_standard_input), None)
@@ -43,6 +49,9 @@ class TestFizzBuzz(unittest.TestCase):
 
         with self.assertRaises(fizzbuzz.ValuesAreNonInteger):
             fizzbuzz.validate_row(['one', '1', '2', '3'])
+
+        with self.assertRaises(fizzbuzz.ValuesAreNonInteger):
+            fizzbuzz.validate_row(null_input)
 
         with self.assertRaises(fizzbuzz.InvalidNumberOfValues):
             fizzbuzz.validate_row(['1', '2', '3'])
